070e59226fee8b432dce661ab05d6ebb19865e62
arttukauppinen <97289054+arttukauppinen@users.noreply.github.com>
2022-01-16 22:15:49 +0200

    updating database.js

--- deno-quizapp/database/database.js
+++ deno-quizapp/database/database.js
@@ -1,15 +1,6 @@
 import { Pool } from "../deps.js";
 
 const CONCURRENT_CONNECTIONS = 2;
-const connectionPool = new Pool({
-  hostname: "balarama.db.elephantsql.com",
-  database: "uacmjhab",
-  user: "uacmjhab",
-  password: "Nd4kAXw9VTozV790OQ1keyZNFObyrPdj",
-  port: 5432,
-}, CONCURRENT_CONNECTIONS);
-
-const CONCURRENT_CONNECTIONS = 2;
 const connectionPool = new Pool({}, CONCURRENT_CONNECTIONS);
 
 const executeQuery = async (query, ...args) => {
